ca86f380c4cf85c5bfa5f80a8575074fb8ebdaf6
Akila De Silva <akiladesilva97@gmail.com>
2025-10-05 16:47:34 +0100

    first commit

---
+++ WebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected ApplicationDbContext()
+        {
+        }
+
+        public DbSet<Cateroy> Cateroys { get; set; }
+    }
+}
